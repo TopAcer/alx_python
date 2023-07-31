83e2715739f0303741395d731d1933c1b02a4d89
TopAcer <askforlegend@gmail.com>
2023-08-01 00:16:05 +0100

    Completed a source code.

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,8 @@
-word = "Holberton"
-print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]))
+word1 = "Holberton"
+word2 = "School"
+print("First 3 letters: {}".format(word1[:3]))
+print("Last 2 letters: {}".format(word1[7:]))
+print("Middle word: {}".format(word1[1:8]))
+print("First 3 letters: {}".format(word2[:3]))
+print("Last two letters: {}".format(word2[4:]))
+print("Middel letters: {}".format(word2[1:5]))
