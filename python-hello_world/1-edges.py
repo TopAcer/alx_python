35b09be506a4417df46a433ccb5e498bed001b70
TopAcer <askforlegend@gmail.com>
2023-08-17 22:50:46 +0100

    Completed a source code while employing the print functiions to each line.

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
-word1 = "Holberton"
+word = "Holberton"
 print("First 3 letters: {}\n".format(word1[:3]))
 print("Last 2 letters: {}\n".format(word1[7:]))
 print("Middle word: {}\n".format(word1[1:8]))
